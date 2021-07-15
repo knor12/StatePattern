6563e400a6752dc3fb3cedce4f624e662322c16a
noreddine kessa <nkessa@lear.com>
2021-07-15 15:27:15 +0100

    no need to include stdlib in logs if it is not going to be used.

--- log.h
+++ log.h
@@ -1,11 +1,12 @@
 #ifndef LOG_H
 #define LOG_H
 
-#include <stdlib.h> 
+
 
 #define ENABLE_LOG 1
 
 #if ENABLE_LOG == 1
+	#include <stdlib.h> 
     #define LOG(x) std::cout << (x)
     #define LOGLN(x) LOG(x) << "\n"
 #elif ENABLE_LOG == 0
